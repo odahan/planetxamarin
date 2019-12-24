97fd7ad5598ee4d98d6d5684076816b69d15c0d2
odahan <odahan@gmail.com>
2019-12-24 02:36:47 +0100

    Update OlivierDahan.cs

--- src/Firehose.Web/Authors/OlivierDahan.cs
+++ src/Firehose.Web/Authors/OlivierDahan.cs
@@ -10,14 +10,14 @@ namespace Firehose.Web.Authors
         public string LastName => "Dahan";
         public string ShortBioOrTagLine => "Consultant Architecte C# XAML MVVM";
         public string StateOrRegion => "France";
-        public string EmailAddress => "odahan@gmail.com";
+        public string EmailAddress => "odahan@e-naxos.com";
         public string TwitterHandle => "odenaxos";
-        public string GravatarHash => "7a41b85a489f96b2d48fb6345d5ef31a";
+        public string GravatarHash => "f8a39142356ff7c473401c2408c353f8";
         public string GitHubHandle => "odahan";
         public GeoPosition Position => new GeoPosition(47.99729, 2.736291);
 
-        public Uri WebSite => new Uri("http://e-naxos.com/blog");
-        public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://www.e-naxos.com/blog/syndication.axd"); } }
+        public Uri WebSite => new Uri("https://e-naxos.com/blog");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://www.e-naxos.com/blog/syndication.axd"); } }
         public string FeedLanguageCode => "fr";
     }
 }
